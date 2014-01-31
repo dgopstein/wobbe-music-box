a95c2eee712fac817b3968252d5186dc9e48c0bc
Dan Gopstein <dan@gopstein.com>
2014-01-30 21:14:28 -0500

    Basic file upload

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -13,4 +13,22 @@ object Application extends Controller {
     Ok(views.html.index("User logged in: " + id))
   }
 
+  def upload = Action {
+    Ok(views.html.upload())
+  }
+
+  def receive = Action(parse.multipartFormData) { request =>
+    request.body.file("picture").map { picture =>
+        import java.io.File
+            val filename = picture.filename 
+            val contentType = picture.contentType
+            picture.ref.moveTo(new File("/tmp/picture"))
+            Ok("File uploaded")
+    }.getOrElse {
+        Redirect(routes.Application.index).flashing(
+                "error" -> "Missing file"
+                )
+    }
+  }
+
 }
--- app/controllers/Audio.scala
+++ app/controllers/Audio.scala
@@ -1,13 +1,13 @@
-package controllers
-
-import javafx.scene.media.Media
-import javafx.scene.media.MediaPlayer
-
-object Audio extends Controllers {
-
-  def playMp3(mp3: String) {
-    var hit: Media = new Media(mp3);
-    var mediaPlayer: MediaPlayer = new MediaPlayer(hit);
-    mediaPlayer.play();
-  }
-}
+//package controllers
+//
+//import javafx.scene.media.Media
+//import javafx.scene.media.MediaPlayer
+//
+//object Audio extends Controllers {
+//
+//  def playMp3(mp3: String) {
+//    var hit: Media = new Media(mp3);
+//    var mediaPlayer: MediaPlayer = new MediaPlayer(hit);
+//    mediaPlayer.play();
+//  }
+//}
