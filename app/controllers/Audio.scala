838e178f1278bbd7bc7ae40aae070bcbd6943f5d
Paul Lam <paul@quantisan.com>
2014-01-30 21:37:34 -0500

    audio play working

--- app/controllers/Audio.scala
+++ app/controllers/Audio.scala
@@ -3,9 +3,9 @@ package controllers
 import javazoom.jl.player.Player
 import java.io.FileInputStream
 
-object Audio extends Controllers {
+object Audio {
   def playMp3(mp3: String) {
-    val fis  = new FileInputStream("mysong.mp3")
+    val fis  = new FileInputStream(mp3)
     val playMP3 = new Player(fis)
     playMP3.play()
   }
